bd5bf17ae21f6c4fb0db7fb2d6047022c8fefbdd
lucifer666 <philip_genev@abv.bg>
2016-03-12 01:05:24 +0200

    First commit

---
+++ leaderboard/leaderboard.js
@@ -0,0 +1,14 @@
+PlayersList = new Mongo.Collection('players');
+
+
+if (Meteor.isClient)
+{
+	//the code runs only on the client
+
+}
+
+if (Meteor.isServer)
+{
+	//the code runs only on the server
+
+}
